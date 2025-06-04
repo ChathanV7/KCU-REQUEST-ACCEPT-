130a79da58beec3c97bbab694b75dfc30ee07e17
FrazBoy <91239698+FrazBoyv1@users.noreply.github.com>
2025-06-05 01:44:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22834593"))
+API_HASH = environ.get("API_HASH", "f400bc1d1baeb9ae93014ce3ee5ea835")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8124671978:AAE3s4intPbSynlaaxEedMDuMHJyIUGWuzw")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002545709718"))
+ADMINS = int(environ.get("ADMINS", "7579162275"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://AOMusic:AOMusic@cluster0.sibxiqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
