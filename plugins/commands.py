61cb0aba5e3c33ba77ce989e4054119d7797ed9e
TOVINO <m2185351@gmail.com>
2026-01-20 17:53:16 +0530

    Update bot links and message formatting

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,10 +20,10 @@ async def start_message(c,m):
         caption=f"<b>Hello {m.from_user.mention} 👋\n\nI 𝖨 𝖼𝖺𝗇 𝖺𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝖼𝖺𝗅𝗅𝗒 𝖺𝗉𝗉𝗋𝗈𝗏𝖾 𝗇𝖾𝗐 𝖺𝗌 𝗐𝖾𝗅𝗅 𝖺𝗌 𝗉𝖾𝗇𝖽𝗂𝗇𝗀 𝗃𝗈𝗂𝗇 𝗋𝖾𝗊𝗎𝖾𝗌𝗍 𝗂𝗇 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝗈𝗋 𝗀𝗋𝗈𝗎𝗉𝗌 \n\n𝖩𝗎𝗌𝗍 𝖺𝖽𝖽 𝗆𝖾 𝗂𝗇 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌 𝗐𝗂𝗍𝗁 𝗉𝖾𝗋𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝗍𝗈 𝖺𝖽𝖽 𝗇𝖾𝗐 𝗆𝖾𝗆𝖻𝖾𝗋𝗌.\n\nFor All Pending Join Request Use - /accept</b>",
         reply_markup=InlineKeyboardMarkup(
             [[
-                InlineKeyboardButton('♡ DEVOLOPER / BOT UPDATES ♡', url='https://t.me/Cyber_Fcracker')
+                InlineKeyboardButton('♡ DEVOLOPER / BOT UPDATES ♡', url='https://t.me/KCUxBOTS')
             ],[
-                InlineKeyboardButton("✘ ADD TO GROUP ✘", url='https://telegram.me/AutoaacceptingBot?startgroup=true&admin=invite_users'),
-                InlineKeyboardButton("✘ ADD TO CHANNEL ✘", url='https://telegram.me/AutoaacceptingBot?startchannel=true&admin=invite_users')
+                InlineKeyboardButton("✘ ADD TO GROUP ✘", url='https://telegram.me/KCU_AUTOACCEPT_BOT?startgroup=true&admin=invite_users'),
+                InlineKeyboardButton("✘ ADD TO CHANNEL ✘", url='https://telegram.me/KCU_AUTOACCEPT_BOT?startchannel=true&admin=invite_users')
             ]]
         )
     )
